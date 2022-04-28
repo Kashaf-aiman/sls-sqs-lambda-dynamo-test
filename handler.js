b855bc5a0f54442a306bb6f64edb2d2d28086e6f
Kashaf <aimankashaf33@gmail.com>
2022-04-29 01:02:18 +0530

    myfirst commit

--- handler.js
+++ handler.js
@@ -79,35 +79,3 @@ module.exports.saveDynamo = async (event) => {
 
 
   
-
-// module.exports.hello =  (event, context , callback) => {
-
-//   const params = {
-//     MessageBody: 'Hola',
-//     QueueUrl: QUEUE_URL
-//   };
-//   sqs.sendMessage(params,function(err,data) {
-//     if (err) {
-//       console.log('error:', 'Fail Send Message' + err);
-
-//       const response = {
-//         statusCode: 500,
-//         body:JSON.stringify({
-//           message: 'ERROR'
-//         })
-//       };
-
-//       callback(null, response);
-//     }  else {
-//         console.log('data:', data.MessageId);
-
-//         const response = {
-//           statusCode: 200,
-//           body: JSON.stringify({
-//             message:data.MessageId
-//           })
-//         };
-//         callback(null, response);
-//     }
-//   }) 
-// };
